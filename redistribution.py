3c75b6f4b1d4486c1c67ff2af651c8adfb5ed37b
AVarsi88 <avarsi88@liverpool.ac.uk>
2023-04-04 14:49:16 +0100

    new branch called distributed fixed size redistribution

--- redistribution.py
+++ redistribution.py
@@ -1,16 +1,14 @@
 import numpy as np
 from mpi4py import MPI
-from discretesampling.domain.decision_tree.util import pad, restore
-from discretesampling.base.algorithms.smc_components.distributed_variable_size_redistribution.rotational_nearly_sort import rot_nearly_sort
-from discretesampling.base.algorithms.smc_components.distributed_variable_size_redistribution.rotational_split import rot_split
+from .rotational_nearly_sort import rot_nearly_sort
+from .rotational_split import rot_split
 
 
 def sequential_redistribution(x, ncopies):
     return np.repeat(x, ncopies, axis=0)
 
 
-def redistribute(particles, ncopies):
-    x = pad(particles)
+def fixed_size_redistribution(x, ncopies):
 
     if MPI.COMM_WORLD.Get_size() > 1:
         x, ncopies = rot_nearly_sort(x, ncopies)
@@ -18,32 +16,4 @@ def redistribute(particles, ncopies):
 
     x = sequential_redistribution(x, ncopies)
 
-    particles = restore(x, particles)
-
-    return particles
-
-
-def centralised_redistribution(particles, ncopies):
-    comm = MPI.COMM_WORLD
-    N = len(ncopies) * comm.Get_size()
-    rank = comm.Get_rank()
-
-    x = pad(particles)
-
-    all_ncopies = np.zeros(N, dtype=ncopies.dtype)
-    all_x = np.zeros([N, x.shape[1]], dtype=x.dtype)
-    ncopies_MPI_dtype = MPI._typedict[ncopies.dtype.char]
-    x_MPI_dtype = MPI._typedict[x.dtype.char]
-
-    comm.Gather(sendbuf=[ncopies, ncopies_MPI_dtype], recvbuf=[all_ncopies, ncopies_MPI_dtype], root=0)
-    comm.Gather(sendbuf=[x, x_MPI_dtype], recvbuf=[all_x, x_MPI_dtype], root=0)
-
-    if rank == 0:
-        all_x = sequential_redistribution(all_x, all_ncopies)
-
-    comm.Scatter(sendbuf=[all_x, x_MPI_dtype], recvbuf=[x, x_MPI_dtype], root=0)
-
-    particles = restore(x, particles)
-
-    return particles
-
+    return x
